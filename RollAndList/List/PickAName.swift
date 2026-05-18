0552c7d7eaf2fcd35e63a3743b475ad7e16bb0cc
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-18 21:00:55 +0530

    added list view

---
+++ RollAndList/List/PickAName.swift
@@ -0,0 +1,18 @@
+//
+//  PickAName.swift
+//  RollAndList
+//
+//  Created by Venkatesh Munaga on 18/05/26.
+//
+
+import SwiftUI
+
+struct PickAName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickAName()
+}
