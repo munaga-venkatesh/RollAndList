43fbfa6f46fec5ac88db283c1355bd6d2bc6c145
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-18 20:30:24 +0530

    Initial Commit

---
+++ RollAndList/RollAndListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollAndListApp.swift
+//  RollAndList
+//
+//  Created by Venkatesh Munaga on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RollAndListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
