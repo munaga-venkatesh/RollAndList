0e800e73c9c6fbe4acd6a7cec3b9d4a772e511ac
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-18 21:01:31 +0530

    adde a picak a name view

--- RollAndList/List/PickAName.swift
+++ RollAndList/List/PickAName.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct PickAName: View {
+    @State private var names: [String] = ["Bob", "Mandi", "Summer", "John"]
+    @State private var nameToAdd: String = ""
+    @State private var pickedName: String = ""
+    @State private var shouldRemoveName: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {            
+            List(names, id: \.description) { name in
+                Text(name)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            TextField("Add a name", text: $nameToAdd)
+                .autocorrectionDisabled(true)
+                .onSubmit {
+                    if !nameToAdd.isEmpty && nameToAdd.trimmingCharacters(in: .whitespaces).count > 3 {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Divider()
+            
+            Toggle("Remove a name", isOn: $shouldRemoveName)
+                .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+            
+            Button {
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                    if shouldRemoveName {
+                        names.removeAll { name in
+                            return (name == randomName)
+                        }
+                    }
+                }
+            } label: {
+                Text("Pick random name")
+                    .font(.title2)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+
+                
+        }
+        .padding()
     }
 }
 
